166b15917cbf14b4d8d171bd4a1ae963b91ddd87
jeffondev <jeffondev@gmail.com>
2022-05-01 14:58:55 +0900

    25083

---
+++ 25083/25083/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  25083
+//
+//  Created by 김진호 on 2022/05/01.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    cout<<"         ,r\'\"7"<<endl;
+    cout<<"r\`-_   ,\'  ,/"<<endl;
+    cout<<" \\. \". L_r\'"<<endl;
+    cout<<"   \`~\\\/"<<endl;
+    cout<<"      |"<<endl;
+    cout<<"      |"<<endl;
+    return 0;
+}
+
