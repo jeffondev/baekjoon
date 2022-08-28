5301bf729a84ed5ba9b0951f959cbaccbbfd0f3b
jeffondev <jeffondev@gmail.com>
2022-08-28 19:18:30 +0900

    2869

--- 2869/2869/main.cpp
+++ 2869/2869/main.cpp
@@ -1,20 +1,16 @@
 //
 //  main.cpp
-//  10757
+//  2869
 //
 //  Created by 김진호 on 2022/08/28.
 //
 
 #include <iostream>
 using namespace std;
-
-int Add(int a, int b) {
-    return a+b;
-}
-
 int main(int argc, const char * argv[]) {
-    int n;
-    cin>>n;
     
+    int A,B,V;
+    cin>>A>>B>>V;
+    cout<<(V-B-1) / (A-B) + 1;
     return 0;
 }
