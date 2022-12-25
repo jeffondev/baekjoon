33c9da48d8a1726d2d3f3f6e0a91a024416d51ca
jeffondev <jeffondev@gmail.com>
2022-12-25 14:41:37 +0900

    10407

---
+++ 10407/10407/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  10407
+//
+//  Created by 김진호 on 2022/12/25.
+//
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    long long n;
+    cin>>n;
+    cout<<(n==1 ? 2 : 1);
+}
